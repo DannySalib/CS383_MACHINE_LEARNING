10d965756e4c1bff8ddca02d3b0a9cab4b619ed6
DannySalib <dhs12241@gmail.com>
2025-01-16 20:41:39 -0500

    finished lab 2

--- labs/l2/main.py
+++ labs/l2/main.py
@@ -1,7 +1,7 @@
 # Danny Salib
 # Jan 15 2025
-# CS383 ML 
-# Lab 2
+# CS383 ML Lab 2
+# GNU bash, version 5.2.21(1)-release (x86_64-pc-linux-gnu)
 
 import os 
 import numpy as np 
@@ -12,8 +12,9 @@ import matplotlib.pyplot as plt
 PATH = './yalefaces'
 
 def main():
-	
-	# STEP 1 - read each image, resize, flatten, and append to our data matrix
+	print('PART 1')
+	print('Initializing Data...')
+	# TODO - read each image, resize, flatten, and append to our data matrix
 	X = [] # initialize data matrix 
 
 	files = os.listdir(PATH)
@@ -32,10 +33,8 @@ def main():
 		else:
 			X = np.append(X, Xi, axis=0)
 
-	# Plot initial matrix 
-	MyUtil.plot_matrix(X, 'Initial Data')
-
-		# Reduce the data to 2D using PCA 
+	print("Reducing Data via PCA...")
+	# TODO Reduce the data to 2D using PCA 
 	# Zero mean data 
 	X = X - np.mean(X)
 
@@ -53,18 +52,25 @@ def main():
 	W = eigenvectors[:, :2]
 	Z = X @ W 
 
+	title = 'PCA_(2D)_data.png'
+	print(f'Saving Resulting Data as \"{title}\"')
 	# Plot resulting matrix 
-	MyUtil.plot_matrix(Z, 'PCA (2D) Data')
+	MyUtil.plot_matrix(Z, title)
 
+	print('PART 2')
+	print('Initialzing Image Data...')
 	# project subject02.centerlight onto the k most important principle components
 	matrix = Image.open(f'{PATH}/subject02.centerlight')
 	matrix = matrix.resize((40, 40))
 	
+	title = 'part2_inital_image.png'
+	print(f'Saving Image\'s Initial Data as \"{title}\"')
 	# Save initial image
 	plt.figure()
-	plt.imshow(matrix)
-	plt.savefig('initial.png')
+	plt.imshow(matrix, cmap='gray')
+	plt.savefig(title)
 	
+	print(f'Compressing w/ PCA via Lossy Compression')
 	matrix = np.asmatrix(matrix.getdata(),dtype=np.float64) # flatten 
 	# Zero mean image 
 	matrix_mean = np.mean(matrix)
@@ -79,20 +85,25 @@ def main():
 	# Dimensionality = k
 	k = np.searchsorted(cumulative_sum, threshold) + 1  # +1 because indices are 0-based
 	W = eigenvectors[:, :k]
-
-	Z = np.dot(matrix, W)
 	
-	Xhat = Z @ W.T
+	Z = np.dot(matrix, W) # result 
+	
+	Xhat = Z @ W.T # reconstruction of data 
 
 	# Undo zero mean and convert to 40x40 image 
 	Xhat += matrix_mean
 	Xhat = Xhat.reshape((40, 40))
 
+
+	title = 'part2_processed_image.png'
+	print(f'Saving Image\'s Processed Data as \"{title}\"')
 	# Save result
 	plt.figure()
-	plt.imshow(Xhat)
-	plt.savefig('result.png')
+	plt.imshow(Xhat, cmap='gray')
+	plt.savefig(title)
 
 	
 if __name__ == '__main__':
+	print(f'INITIAL DATA PATH: {PATH}')
 	main()
+	print('FINISHED!')
