fca57bb00a060d256deb2e7aefa341221542ed4a
DannySalib <dhs12241@gmail.com>
2025-01-30 12:58:26 -0500

    finished lab 3

--- labs/l3/main.py
+++ labs/l3/main.py
@@ -27,7 +27,40 @@ def main():
     
     # Shuffle Data
     df = df.sample(frac=1)
+    df.insert(0, 0, 1) # we need an extra feature for the weight intercept 
 
+    # Split into training data (2/3) and test data (1/3)
+    split_index = (len(df) * 2) // 3
+    training_df = df.iloc[:split_index, :]
+    validating_df = df.iloc[split_index:, :]
+
+    # Target values
+    Y_tr = training_df['charges'].to_numpy()
+    Y_v = validating_df['charges'].to_numpy()
+    X_tr = training_df.drop('charges', axis=1).to_numpy()
+    X_v = validating_df.drop('charges', axis=1).to_numpy()
+
+    # Calculate our weight 
+    W = np.linalg.pinv(X_tr) @ Y_tr
+
+    # Calculate results
+    result = lambda X: W[0] + np.dot(X[:, 1:], W[1:])
+    Yhat_tr = result(X_tr)
+    Yhat_v = result(X_v)
+    
+    # Display results 
+    print(f'Training RMSE: {RMSE(Y_tr, Yhat_tr)}')
+    print(f'Validation RMSE: {RMSE(Y_v, Yhat_v)}')
+    print(f'Training SMAPE (%): {SMAPE(Y_tr, Yhat_tr)}')
+    print(f'Validation SMAPE (%): {SMAPE(Y_v, Yhat_v)}')
+
+def RMSE(Y, Yhat):
+    return np.sqrt(np.mean((Y - Yhat) ** 2))
+
+def SMAPE(Y, Yhat):
+   numerator = np.abs(Y - Yhat)
+   denominator = (np.abs(Y) + np.abs(Yhat))
+   return np.mean(numerator / denominator) * 100  # Convert to percentage
 
 if __name__ == '__main__':
     main()
